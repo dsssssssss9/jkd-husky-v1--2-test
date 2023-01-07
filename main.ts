10d4e0f9eb09549c3a981042c598ea3de22cb711
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-07 22:10:58 +0000

    name 3 learned tags

--- main.ts
+++ main.ts
@@ -10,6 +10,9 @@ basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.forever(function () {
     huskylens.request()
+    huskylens.writeName(1, "Kirk")
+    huskylens.writeName(2, "Spock")
+    huskylens.writeName(3, "McCoy")
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         basic.showLeds(`
             . . . . .
